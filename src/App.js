c9d9698d06d72ad716d4ab275ba38eecaccb6390
Kevin Gomes <kego@kegomac.local>
2020-09-10 11:45:35 -0500

    Added functioanlity to invoke endpoints rotected by ALB

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import React from 'react';
+import axios from 'axios';
+
 import logo from './logo.svg';
 import './App.css';
 
+const resourceUrl1 = 'https://oidc-663207740.us-west-2.elb.amazonaws.com/headers';
+const resourceUrl2 = 'https://oidc-663207740.us-west-2.elb.amazonaws.com/second';
+
 function App() {
+  const invokeMicroservice = (endpoint) => {
+    console.log('invokeMicroservice: ');
+
+    axios.get(endpoint, {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      }
+    }).then(function (response) {
+      // handle success
+      console.log(response);
+    })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Testing OIDC Authentication & Authorization via ALB
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => { invokeMicroservice(resourceUrl1); } }>Invoke Endpoint 1</button>
+        <button onClick={() => { invokeMicroservice(resourceUrl2); } }>Invoke Endpoint 2</button>
       </header>
     </div>
   );
